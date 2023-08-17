c9a81e91955073612e343a6d1243241a7b5ef5f1
Elmira <89019222+NalbandyanElmira@users.noreply.github.com>
2023-08-17 19:51:17 +0400

    Update document.cpp

--- src/document.cpp
+++ src/document.cpp
@@ -39,37 +39,37 @@ void Document::remove(const std::string& key, const std::string& value) {
 }
 
 void Document::saveToJSON(const std::string& filename) const {
-    nlohmann::json jsonDocument; 
-    for (const auto& pair : doc) {
-        jsonDocument[pair.first] = pair.second;
-    }
-    std::ofstream fileStream(filename);
-    if (fileStream.is_open()) {
-        fileStream << jsonDocument.dump();  
-        fileStream.close();
-    } else {
-        std::cerr << "Unable to open file for writing." << std::endl;
-    }
+    	nlohmann::json jsonDocument; 
+    	for (const auto& pair : doc) {
+        	jsonDocument[pair.first] = pair.second;
+    	}
+    	std::ofstream fileStream(filename);
+    	if (fileStream.is_open()) {
+        	fileStream << jsonDocument.dump();  
+        	fileStream.close();
+    	} else {
+        	std::cerr << "Unable to open file for writing." << std::endl;
+    	}
 }
 
 void Document::loadFromJSON(const std::string& filename) {
-    std::ifstream fileStream(filename);
-    if (fileStream.is_open()) {
-        nlohmann::json jsonDocument;
-        fileStream >> jsonDocument;
-        for (const auto& item : jsonDocument.items()) {
-            doc[item.key()] = item.value().get<std::string>();
-        }
-        fileStream.close();
-    } else {
-        std::cerr << "Unable to open file for reading." << std::endl;
-    }
+    	std::ifstream fileStream(filename);
+    	if (fileStream.is_open()) {
+        	nlohmann::json jsonDocument;
+        	fileStream >> jsonDocument;
+        	for (const auto& item : jsonDocument.items()) {
+           		doc[item.key()] = item.value().get<std::string>();
+        	}
+        	fileStream.close();
+    	} else {
+        	std::cerr << "Unable to open file for reading." << std::endl;
+    	}
 }
 
 std::string Document::getFieldValue(const std::string& field) const {
-    auto it = doc.find(field);
-    if (it != doc.end()) {
-        return it->second;
-    }
-    return "";
+    	auto it = doc.find(field);
+    	if (it != doc.end()) {
+        	return it->second;
+    	}
+    	return "";
 }
